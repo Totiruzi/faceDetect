da50c2f622de687c879e36936e3888141c8e92e5
Totiruzi <totiruzi@yahoo.com>
2020-08-19 17:52:01 +0300

    Deploy to heroku

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageUrl', {
+      fetch('https://mysterious-coast-30233.herokuapp.com/imageUrl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -103,7 +103,7 @@ class App extends Component {
       .then(response => response.json())
       .then((response) => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://mysterious-coast-30233.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
