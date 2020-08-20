b37cfdc142140733982ec17c158042eeb1d6e810
Totiruzi <totiruzi@yahoo.com>
2020-08-20 09:08:34 +0300

    Updates

---
+++ precache-manifest.e63f6a4f93580cf84e666e8a91f8fff9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33cf2142c049dfce779fb662a41d19c",
+    "url": "/faceDetect/index.html"
+  },
+  {
+    "revision": "608d356ca77f4647e650",
+    "url": "/faceDetect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd4750cf835a4647fb54",
+    "url": "/faceDetect/static/css/main.f6356a0f.chunk.css"
+  },
+  {
+    "revision": "608d356ca77f4647e650",
+    "url": "/faceDetect/static/js/2.c8205804.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceDetect/static/js/2.c8205804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4750cf835a4647fb54",
+    "url": "/faceDetect/static/js/main.c6810029.chunk.js"
+  },
+  {
+    "revision": "fd27524e499a68112f44",
+    "url": "/faceDetect/static/js/runtime-main.afcb8f13.js"
+  }
+]);
